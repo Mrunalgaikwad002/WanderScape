ce4120852fd32c1bd8ea5fdcee280529d1f8cf91
Mrunal Gaikwad <mrunalgaikwad02@gmail.com>
2025-04-16 20:59:19 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 const connectDB = async()=>{
     try{
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        console.log('MONGO_URI from env:', process.env.MONGO_URI);
+;
     }catch(error){
         console.error(`Error: ${error.message}`);
         process.exit(1);
